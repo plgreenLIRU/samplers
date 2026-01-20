9d64099a4fb3afccdd3f9bbab2de1f6e855d9677
Pete Green <p.l.green@liverpool.ac.uk>
2026-01-20 13:33:38 +0000

    now works with 4 chains

--- examples/mh_example.py
+++ examples/mh_example.py
@@ -9,4 +9,6 @@ def logp(params, data):
     return -0.5 * np.log(v) - 1 / (2 * v) * (params[0] - mu)**2
 
 s = MH(log_target=logp)
-all_samples, ar = s.generate_samples(params_current=[[0], [1]], data=None, proposal_width=0.1, n_samples=5000, n_chains=2, plot_live=True)
+all_samples, ar = s.generate_samples(params_current=[[0], [1], [2], [3]],
+                                     data=None, proposal_width=0.1, n_samples=5000,
+                                     n_chains=4, plot_live=True)
